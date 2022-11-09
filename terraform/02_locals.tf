24b4a0f9c0b9c7c180bb7dd9a38962ac35b7a8e4
uturkarslan <ugurturkarslan@gmail.com>
2022-11-09 12:08:07 +0100

    Consider namespaces

--- terraform/02_locals.tf
+++ terraform/02_locals.tf
@@ -1,11 +1,16 @@
 locals {
 
+  # Dashboards - Map of deployments
+  dashboards_deployments = {
+    for deployment in var.deployments : deployment.namespaceName => deployment.deploymentNames
+  }
+
   # Alerts - List of deployments
   alerts_deployments = flatten(
     [
       for namespace in var.deployments : [
         for deploymentName in namespace.deploymentNames : {
-          namespaceName = namespace.namespaceName
+          namespaceName  = namespace.namespaceName
           deploymentName = deploymentName
         }
       ]
