ebf6d6fd1705dcb4be327f8f778c38f9e8f4930a
uturkarslan <ugurturkarslan@gmail.com>
2022-11-13 23:39:34 +0100

    Add us or eu check

--- scripts/02_deploy_newrelic_terraform.sh
+++ scripts/02_deploy_newrelic_terraform.sh
@@ -28,10 +28,16 @@ if [[ $NEWRELIC_ACCOUNT_ID == "" ]]; then
   echo "Define New Relic account ID as an environment variable [NEWRELIC_ACCOUNT_ID]. For example: -> export NEWRELIC_ACCOUNT_ID=xxx"
   exit 1
 fi
+
 # New Relic region
 if [[ $NEWRELIC_REGION == "" ]]; then
   echo "Define New Relic region as an environment variable [NEWRELIC_REGION]. For example: -> export NEWRELIC_REGION=us or export NEWRELIC_REGION=eu"
   exit 1
+else
+  if [[ $NEWRELIC_REGION != "us" && $NEWRELIC_REGION != "eu" ]]; then
+    echo "New Relic region can either be 'us' or 'eu'."
+    exit 1
+  fi
 fi
 
 # New Relic API key
